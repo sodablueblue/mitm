e5a48545c74026c8c4402a2f5ed58e894626f9ab
sodablueblue <zhl65@sodablue.local>
2016-01-24 16:41:52 +0800

    pcap implement

---
+++ index.js
@@ -0,0 +1,7 @@
+var arp = require('./lib/arp.js');
+
+arp.getInstance();//.setSrcIp('192.168.1.1');
+//arp.getInstance().setDstMac('CC:29:F5:CF:2C:F0');
+//arp.getInstance().setDstIp('192.168.1.102');
+
+//arp.getInstance().poision();
